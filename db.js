56b487fcf91736999f879ac516881901cb14ea3e
Nataliaum11 <natalia.usuga0532@unaula.edu.co>
2025-04-16 19:19:01 -0500

    Interfaz Home

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+console.log('Probando la conexión');
+
+const connection = mysql.createConnection({
+  host: 'buu9jmms9fltha4jefno-mysql.services.clever-cloud.com',
+  user: 'uphozyqbhh7vltks',
+  password: 'plO9h9EZ8JhavB1JGXvT',
+  database: 'buu9jmms9fltha4jefno',
+  port: 3306
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err.message);
+  } else {
+    console.log('¡Conexión exitosa a la base de datos!');
+  }
+});
+
+module.exports = connection;
